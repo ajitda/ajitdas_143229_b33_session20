a4fa9a2817fd4357add94ba6d4ef125b42639b02
Ajit Das <ajitdas2900@gmail.com>
2016-10-31 11:17:32 +0600

    autoload method

--- views/SEIP143229/index.php
+++ views/SEIP143229/index.php
@@ -1,9 +1,14 @@
 <?php
 use App\person;
 use App\Student;
+
 //require_once("../../src/BITM/SEIP143229/Person.php");
 //require_once("../../src/BITM/SEIP143229/Student.php");
 function __autoload($className){
+
+
+
+    echo $className."<br>";
     list($ns,$cn) = explode("\\", $className);
         require_once("../../src/BITM/SEIP143229/".$cn.".php");
 }
